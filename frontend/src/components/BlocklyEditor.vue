27dc1591b13d36a480be681b864f2bb14d5e5584
George Soteriou <george.soteriou.1997@gmail.com>
2019-10-18 12:00:28 +0100

    blockly

--- frontend/src/components/BlocklyEditor.vue
+++ frontend/src/components/BlocklyEditor.vue
@@ -12,11 +12,33 @@
 
 <script>
 export default {
+  beforeCreate() {
+    // import blockly files
+    let blocklyScript = document.createElement("script");
+    blocklyScript.setAttribute("src", "/blockly/blockly_compressed.js");
+    document.head.appendChild(blocklyScript);
+
+    let blockScript = document.createElement("script");
+    blockScript.setAttribute("src", "/blockly/blocks_compressed.js");
+    document.head.appendChild(blockScript);
+
+    let javascriptBlocklyScript = document.createElement("script");
+    javascriptBlocklyScript.setAttribute(
+      "src",
+      "/blockly/javascript_compressed.js"
+    );
+    document.head.appendChild(javascriptBlocklyScript);
+
+    let EnglishLangScript = document.createElement("script");
+    EnglishLangScript.setAttribute("src", "/blockly/msg/js/en.js");
+    document.head.appendChild(EnglishLangScript);
+  },
   mounted() {
+    // Actually run blockly
     var workspace = Blockly.inject("blocklyDiv", {
       toolbox: document.getElementById("toolbox")
     });
-    function myUpdateFunction(event) {
+    function myUpdateFunction() {
       var code = Blockly.JavaScript.workspaceToCode(workspace);
       document.getElementById("textarea").value = code;
     }
