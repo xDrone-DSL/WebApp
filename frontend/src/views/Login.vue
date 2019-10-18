c460ff896c2487afec07503daa1c9259a2cdd915
George Soteriou <george.soteriou.1997@gmail.com>
2019-10-18 09:57:23 +0100

    fix login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,14 +3,11 @@
     <v-btn class="ma-4">
       <router-link to="/student">Student</router-link>
     </v-btn>
-    <v-btn class="ma-4">
-      <router-link to="/teacher">Teacher</router-link>
-    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  data() {}
+  data: () => ({})
 };
 </script>
