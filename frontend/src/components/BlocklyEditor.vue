19b9370c0d3fa40b4ba7db086aa77aabf6eefbbc
George Soteriou <george.soteriou.1997@gmail.com>
2019-10-31 14:15:33 +0000

    update toolbox

--- frontend/src/components/BlocklyEditor.vue
+++ frontend/src/components/BlocklyEditor.vue
@@ -1,14 +1,18 @@
 <template>
   <v-layout>
     <div id="toolbox" style="display: none">
-      <block type="fly"></block>
-      <block type="move"></block>
-      <block type="rotate"></block>
+      <category name="xDrone">
+        <block type="fly"></block>
+        <block type="move"></block>
+        <block type="rotate"></block>
+      </category>
     </div>
     <div id="toolbox2" style="display: none">
-      <block type="fly" disabled="true"></block>
-      <block type="move"></block>
-      <block type="rotate"></block>
+      <category name="xDrone">
+        <block type="fly" disabled="true"></block>
+        <block type="move"></block>
+        <block type="rotate"></block>
+      </category>
     </div>
     <div id="blocklyDiv" style=" height: 800px; width: 1200px;"></div>
     <textarea id="textarea" style="height: 800px; width: 300px;"></textarea>
