01a98e1376617290e0ad1ef53cebd34a7f5f1073
Kexin Gu <kexin.gu17@imperial.ac.uk>
2019-12-24 14:50:55 +0000

    Fix enter login when login disabled

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -61,6 +61,9 @@ export default {
   }),
   methods: {
     login() {
+      if (!this.loginEnabled) {
+        return;
+      }
       if (this.teamName === "") {
         this.error = true;
       } else {
