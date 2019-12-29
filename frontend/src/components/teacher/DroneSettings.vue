c987e0ffc43df0c0155e3f4d4f278931f55f2a24
George Soteriou <george.soteriou.1997@gmail.com>
2019-12-29 13:34:12 +0200

    add dialog for new drone

--- frontend/src/components/teacher/DroneSettings.vue
+++ frontend/src/components/teacher/DroneSettings.vue
@@ -23,6 +23,51 @@
               @change="updateDrone(drone.mac, drone.enabled)"
             ></v-switch>
           </v-list-item>
+
+          <v-list-item @click="newDrone = true">
+            <v-list-item-icon>
+              <v-icon>mdi-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>Add new drone</v-list-item-content>
+            <v-dialog v-model="newDrone" max-width="400">
+              <v-card>
+                <v-card-title class="headline">
+                  Add New Drone
+                </v-card-title>
+                <v-card-text>
+                  <v-form ref="form" v-model="valid">
+                    <v-container>
+                      <v-row>
+                        <v-col>
+                          <v-text-field
+                            v-model="newName"
+                            :rules="nameRules"
+                            label="Name"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col>
+                          <v-text-field
+                            v-model="newMac"
+                            :rules="macRules"
+                            label="Mac"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                    <v-spacer></v-spacer>
+                    <v-btn color="success" @click="addDrone()">
+                      Add
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-dialog>
+          </v-list-item>
         </v-list>
 
         <v-card-actions>
@@ -43,7 +88,26 @@ export default {
   data() {
     return {
       show: false,
-      drones: []
+      drones: [],
+      newDrone: false,
+      valid: false,
+      newMac: "",
+      newName: "",
+      nameRules: [
+        v => !!v || "Name is required",
+        name =>
+          !this.drones.find(drone => drone.name === name) ||
+          "Name already exists"
+      ],
+      macRules: [
+        v => !!v || "MAC is required",
+        mac => {
+          const macRegex = /^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$/;
+          return macRegex.test(mac) || "please enter a valid MAC address";
+        },
+        mac =>
+          !this.drones.find(drone => drone.mac === mac) || "Mac already exists"
+      ]
     };
   },
   mounted() {
@@ -58,6 +122,17 @@ export default {
         mac: mac,
         state: !state
       });
+    },
+    addDrone() {
+      if (this.$refs.form.validate()) {
+        socket.emit("SET_DRONE", {
+          mac: this.newMac,
+          name: this.newName,
+          state: true
+        });
+        this.$refs.form.reset();
+        this.newDrone = false;
+      }
     }
   }
 };
